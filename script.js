d92a863ec86979e198f06cf514d7b1649092ae98
femmytedrey <femmytedrey@gmail.com>
2023-08-12 23:34:28 +0100

    progress on project

---
+++ script.js
@@ -0,0 +1,14 @@
+// // document.addEventListener("DOMContentLoaded", function () {
+// //     const menuToggle = document.querySelector(".menu-toggle");
+// //     const bars = document.querySelector(".menu-btn i");
+
+// //     menuToggle.addEventListener("change", function () {
+// //         if (menuToggle.checked) {
+// //             bars.classList.remove("fa-bars");
+// //             bars.classList.add("fa-times");
+// //         } else {
+// //             bars.classList.remove("fa-times");
+// //             bars.classList.add("fa-bars");
+// //         }
+// //     });
+// // });
